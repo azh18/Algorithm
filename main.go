b22da29cb105b1662edfd305e6ae1619d1f8cee3
zbw0046 <zbw0046@gmail.com>
2018-10-17 15:08:47 +0800

    add Longest Increasing Subsequence Algorith

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"github.com/Algorithm/FPTree"
 	"fmt"
+	"github.com/Algorithm/LIS"
 )
 
-func main() {
+func FPTreeTest(){
 	trans := [][]string{{"a", "b", "c", "d", "e", "f", "g", "h"},
 		{"b", "d", "e", "f", "j"},
 		{"a", "f", "g"},
@@ -26,4 +27,16 @@ func main() {
 	fmt.Printf("pattern for trans2 = %v\n", patterns2)
 
 	return
+}
+
+func LISTest(){
+	input := []int{10,22,9,33,21,50,41,47,60,80}
+	nLis, lis := LIS.GetLIS(input)
+	fmt.Printf("The max length of increasing sub-sequence: %d\n", nLis)
+	fmt.Printf("The LIS is %v", lis)
+	return
+}
+
+func main() {
+	LISTest()
 }
